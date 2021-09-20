a3cd06af9fffda19793229e4ec77db0792af5656
Shweta Sheth <shweta08@Shwetas-MacBook-Air.local>
2021-09-20 10:00:56 -0500

    Elevator Task

--- elevator.py
+++ elevator.py
@@ -3,83 +3,79 @@ import time
 
 voice = pyttsx3.init()
 
-try:
-    voice.say("Press u for going up and d for going down")
-    voice.runAndWait()
-    direction = input("Press u for going up and d for going down")
+voice.say("Press u for going up and d for going down")
+voice.runAndWait()
+direction = input("Press u for going up and d for going down")
 
 
-    #print("Elevator Door Opens")
-    voice.say("Elevator Door Opens")
-    voice.runAndWait()
+#print("Elevator Door Opens")
+voice.say("Elevator Door Opens")
+voice.runAndWait()
 
 
-    #print("W e l c o m e  T o  E l e v a t o r")
-    voice.say("Welcome  To  Elevator")
-    voice.runAndWait()
+#print("W e l c o m e  T o  E l e v a t o r")
+voice.say("Welcome  To  Elevator")
+voice.runAndWait()
 
 
-    #print("Elevator Door Closes")
-    voice.say("Elevator Door Closes")
-    voice.runAndWait()
+#print("Elevator Door Closes")
+voice.say("Elevator Door Closes")
+voice.runAndWait()
 
 
-    voice.say("Which floor you are at?")
-    voice.runAndWait()
-    floor = int(input("Which floor you are at?"))
+voice.say("Which floor you are at?")
+voice.runAndWait()
+floor = int(input("Which floor you are at?"))
 
 
-    voice.say("Which floor you want to go at?")
-    voice.runAndWait()
-    dest = int(input("Which floor you want to go at?"))
+voice.say("Which floor you want to go?")
+voice.runAndWait()
+dest = int(input("Which floor you want to go?"))
 
 
-except:
-    #print("please enter an integer")
-    voice.say("please enter an integer")
-    voice.runAndWait()
-
-
-finally:
-    max = 10
-    total = 0
-    if direction == 'u' and dest < 11 and (floor and dest) > 0:
-        total = dest - floor
-        if total > 0:
-            while floor < dest:
-                print(floor)
-                time.sleep(1)
-                floor += 1
-            print(f"Reached  at  floor  {dest}")
-            voice.say(f"Reached  at  floor  {dest}")
-            voice.runAndWait()
-
-            print("Door Opens")
-            voice.say("Door Opens")
-            voice.runAndWait()
-
-        else:
-            #print("Please enter a higher floor number")
-            voice.say("Please enter a higher floor number")
-            voice.runAndWait()
-
-    elif direction == 'd'and dest < 11 and (floor and dest) > 0:
-        total = floor - dest
-        if total > 0:
-            while floor > dest:
-                print(floor)
-                time.sleep(1)
-                floor -= 1
-            print(f"Reached at floor {dest}")
-            voice.say(f"Reached at floor {dest}")
-            voice.runAndWait()
-
-        else:
-            #print("Please enter a lower floor number")
-            voice.say("Please enter a lower floor number")
-            voice.runAndWait()
-    else:
-        #print("Not a floor number")
-        voice.say("Not a floor number")
+max = 10
+total = 0
+if direction == 'u' and dest < 11 and (floor and dest) >= 0:
+    total = dest - floor
+    if total > 0:
+        while floor < dest:
+            print(floor)
+            time.sleep(1)
+            floor += 1
+        print(f"Reached  at  floor  {dest}")
+        voice.say(f"Reached  at  floor  {dest}")
         voice.runAndWait()
-    voice.stop()
+
+        print("Door Opens")
+        voice.say("Door Opens")
+        voice.runAndWait()
+
+    else:
+        #print("Please enter a higher floor number")
+        voice.say("Please enter a higher floor number")
+        voice.runAndWait()
+
+elif direction == 'd'and dest < 11 and (floor and dest) >= 0:
+    total = floor - dest
+    if total > 0:
+        while floor > dest:
+            print(floor)
+            time.sleep(1)
+            floor -= 1
+        print(f"Reached at floor {dest}")
+        voice.say(f"Reached at floor {dest}")
+        voice.runAndWait()
+
+        print("Door Opens")
+        voice.say("Door Opens")
+        voice.runAndWait()
+
+    else:
+        #print("Please enter a lower floor number")
+        voice.say("Please enter a lower floor number")
+        voice.runAndWait()
+else:
+    #print("Not a floor number")
+    voice.say("Not a floor number")
+    voice.runAndWait()
+voice.stop()
